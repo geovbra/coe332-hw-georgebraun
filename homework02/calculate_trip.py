6e75f5b6104c00a1555c018a5c9687bbe48364be
George Braun <georgevbraun@utexas.edu>
2022-02-08 20:39:12 -0600

    fixed small error in calculate_trip.py (radius argument in calc_gcd removed)

--- homework02/calculate_trip.py
+++ homework02/calculate_trip.py
@@ -3,7 +3,7 @@ import math
 
 mars_radius = 3389.5    # km
 
-def calc_gcd(latitude_1: float, longitude_1: float, latitude_2: float, longitude_2: float, mars_radius: float) -> float:
+def calc_gcd(latitude_1: float, longitude_1: float, latitude_2: float, longitude_2: float) -> float:
     lat1, lon1, lat2, lon2 = map( math.radians, [latitude_1, longitude_1, latitude_2, longitude_2] )
     d_sigma = math.acos( math.sin(lat1) * math.sin(lat2) + math.cos(lat1) * math.cos(lat2) * math.cos(abs(lon1-lon2)))
     return ( mars_radius * d_sigma )
