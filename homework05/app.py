c627d1f6118cca2f32ab487ef4b1d3735a34f3cc
geovbra <georgevbraun@utexas.edu>
2022-04-05 08:29:41 -0500

    added docstrings & type annotations to app.py

--- homework05/app.py
+++ homework05/app.py
@@ -10,13 +10,26 @@ app = Flask(__name__)
 rd=redis.Redis(host=sys.argv[1], port=6379)
 
 @app.route('/data', methods = ['GET'])
-def data_post():
+def data_post() -> str:
+    """
+    Communicates with redis container, returning meteorite landing data that was previously stored in a database.
+    
+    Returns:
+        A formatted string representing a json list of meteorite data, or a string prompting the user to load data to
+        the redis database if none exists.
+    """
     if rd.get("ml_data") == None:
         return ("Please use this route with a post command to load data first\n")
     return jsonify(json.loads(rd.get("ml_data")))
 
 @app.route('/data', methods = ['POST'])
-def data_load():
+def data_load() -> str:
+    """
+    Communicates with redis container, loading meteorite landing data and storing it in the redis database.
+    
+    Returns:
+        A string notifying the user that the meteorite landing data has been successfully loaded and stored.
+    """
     with open('ML_Data_Sample.json', 'r') as f:
         ml_data = json.load(f)
     
